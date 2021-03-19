4ecce8b6e78dc693deaf13d63f6fde559fc1f614
julianreumann <58084833+julianreumann@users.noreply.github.com>
2021-03-19 15:16:15 +0100

    TestCase #1 difference between grades // change into "Return Value without rounded numbers" TestCase #2 value of grades less 38 TestCase #3 First parameter too high TestCase #4 First parameter too low TestCase #5 Grades too high TestCase #6 Grades too low TestCase #7 Size of array while transformation TestCase #8 Return Value with rounded numbers

--- src/test/java/TestResult.java
+++ src/test/java/TestResult.java
@@ -5,25 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class TestResult {
-    @BeforeAll
-    static void init() {
-    }
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @DisplayName("#1 difference between grades")
+    @DisplayName("#1 Return Value without rounded numbers")
     @Test
-    public void testGradingStudents_DifferenceGrades() {
-        List<Integer> result = Arrays.asList(4, 73, 67, 84, 88);
+    public void testGradingStudents_ReturnValue() {
+        List<Integer> result = Arrays.asList(4, 72, 65, 80, 90);
         List<Integer> calc = Result.gradingStudents(result);
-        List<Integer> expected = Arrays.asList(75, 67, 85, 90);
+        List<Integer> expected = Arrays.asList(72, 65, 80, 90);
         assertEquals(expected, calc);
     }
     @DisplayName("#2 value of grades less 38")
@@ -34,11 +25,10 @@ public class TestResult {
         List<Integer> expected = Arrays.asList(33, 24, 12, 13);
         assertEquals(expected, calc);
     }
-
     @DisplayName("#3 First parameter too high!")
     @Test
     public void testGradingStudents_FirstParameterTooHigh() {
-        List<Integer> result = Arrays.asList(-1, 73, 67, 38, 33);
+        List<Integer> result = Arrays.asList(66, 73, 67, 38, 33);
         List<Integer> calc = Result.gradingStudents(result);
         List<Integer> expected = new ArrayList<>();
         assertEquals(expected, calc);
@@ -46,12 +36,11 @@ public class TestResult {
     @DisplayName("#4 First parameter too low!")
     @Test
     public void testGradingStudents_FirstParameterTooLow() {
-        List<Integer> result = Arrays.asList(61, 73, 67, 38, 33);
+        List<Integer> result = Arrays.asList(-1, 73, 67, 38, 33);
         List<Integer> calc = Result.gradingStudents(result);
         List<Integer> expected = new ArrayList<>();
         assertEquals(expected, calc);
     }
-
     @DisplayName("#5 Grades too high")
     @Test
     public void testGradingStudents_GradesTooHigh() {
@@ -68,8 +57,6 @@ public class TestResult {
         List<Integer> expected = Arrays.asList(null,67,40,33);
         assertEquals(expected, calc);
     }
-
-
     @DisplayName("#7 Size of array while transformation")
     @Test
     public void testGradingStudents_SizeOfArrayWhileTransformation() {
@@ -78,7 +65,14 @@ public class TestResult {
         List<Integer> expected =Arrays.asList(33, 67, 40, 33);
         assertEquals(expected.size(), calc.size());
     }
-
+    @DisplayName("#8 Return Value with rounded numbers")
+    @Test
+    public void testGradingStudents_ReturnValueRoundedNumbers() {
+        List<Integer> result = Arrays.asList(4, 73, 38, 88, 99);
+        List<Integer> calc = Result.gradingStudents(result);
+        List<Integer> expected = Arrays.asList(75, 40, 90, 100);
+        assertEquals(expected, calc);
+    }
 
 
 
